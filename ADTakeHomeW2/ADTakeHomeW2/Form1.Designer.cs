ad944db29aa5dbdfb75458da1e083bef7a4916b1
Kwandy <124193467+OoMyGit@users.noreply.github.com>
2023-03-12 23:23:05 +0700

    Add files via upload

---
+++ ADTakeHomeW2/ADTakeHomeW2/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ADTakeHomeW2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.kotak1 = new System.Windows.Forms.TextBox();
+            this.kotak2 = new System.Windows.Forms.TextBox();
+            this.kotak3 = new System.Windows.Forms.TextBox();
+            this.kotak4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Phone Number";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(587, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 31);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // kotak1
+            // 
+            this.kotak1.Location = new System.Drawing.Point(181, 27);
+            this.kotak1.Name = "kotak1";
+            this.kotak1.Size = new System.Drawing.Size(470, 26);
+            this.kotak1.TabIndex = 5;
+            // 
+            // kotak2
+            // 
+            this.kotak2.Location = new System.Drawing.Point(181, 74);
+            this.kotak2.Name = "kotak2";
+            this.kotak2.Size = new System.Drawing.Size(470, 26);
+            this.kotak2.TabIndex = 6;
+            // 
+            // kotak3
+            // 
+            this.kotak3.Location = new System.Drawing.Point(181, 123);
+            this.kotak3.Name = "kotak3";
+            this.kotak3.Size = new System.Drawing.Size(470, 26);
+            this.kotak3.TabIndex = 7;
+            // 
+            // kotak4
+            // 
+            this.kotak4.Location = new System.Drawing.Point(181, 169);
+            this.kotak4.Name = "kotak4";
+            this.kotak4.Size = new System.Drawing.Size(470, 26);
+            this.kotak4.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(702, 407);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 31);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.kotak4);
+            this.Controls.Add(this.kotak3);
+            this.Controls.Add(this.kotak2);
+            this.Controls.Add(this.kotak1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox kotak1;
+        private System.Windows.Forms.TextBox kotak2;
+        private System.Windows.Forms.TextBox kotak3;
+        private System.Windows.Forms.TextBox kotak4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ADTakeHomeW2/ADTakeHomeW2/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADTakeHomeW2
+{
+    public partial class Form1 : Form
+    {
+        string age;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Convert.ToInt32(kotak2.Text) >= 18)
+            {
+                age = "You are an adult";
+            }
+            else
+            {
+                age = "You are a minor";
+            }
+            MessageBox.Show("Name : " + kotak1.Text + "\nEmail : " + kotak3.Text + "\nPhone Number : " + kotak4.Text + "\n" + age, "Biodata", MessageBoxButtons.OK, MessageBoxIcon.Information) ;
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            kotak1.Clear();
+            kotak2.Clear();
+            kotak3.Clear();
+            kotak4.Clear();
+        }
+    }
+}
